30d099539de4aafce4a661036e9b2cd8ec85c7bc
Ves <veselynk@gmail.com>
2023-10-01 22:13:04 +0300

    feat(theme): add theme setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,42 @@ import Search from './components/Search/Search.jsx';
 import { useSelector } from 'react-redux';
 import Loader from './components/Loader/Loader';
 import { getWeatherInfo } from './redux/slices/weatherSlice';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { getTheme } from './redux/slices/themeSlice';
 
 export const App = () => {
     // const weatherInfo = useWeatherStore((state) => state.weatherInfo);
     const weatherInfo = useSelector((state) => getWeatherInfo(state));
+    const theme = useSelector((state) => getTheme(state));
 
     return (
         <>
-            <Loader />
-            <NavBar />
-            <Search />
-            {weatherInfo === null ? (
-                <Box
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        width: '100%',
-                        height: '75vh',
-                    }}
-                >
-                    <Typography variant='h2'>
-                        Select a city to get started!
-                    </Typography>
-                </Box>
-            ) : (
-                <>
-                    <Dashboard />
-                </>
-            )}
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Loader />
+                <NavBar />
+                <Search />
+                {weatherInfo === null ? (
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            width: '100%',
+                            height: '75vh',
+                        }}
+                    >
+                        <Typography variant='h2'>
+                            Select a city to get started!
+                        </Typography>
+                    </Box>
+                ) : (
+                    <>
+                        <Dashboard />
+                    </>
+                )}
+            </ThemeProvider>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import weatherStore from './redux/weatherStore.js';
+import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={weatherStore}>
+    <Provider store={store}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
